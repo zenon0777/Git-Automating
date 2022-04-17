c11c2c530a371ac060f645d819b59999299990fc
DAIFI-A <abderrahmane.daifi-etu@etu.univh2c.ma>
2022-04-18 07:03:06 +0800

    first commit using script

--- automate.sh
+++ automate.sh
@@ -7,6 +7,6 @@ read -p {"your message"} message
 git init
 git checkout -b $branche
 git add .
-git commit -m $message
+git commit -m "$message"
 git remote add $repo git@github.com:DAIFI-A/$repo.git
 git push -u $repo $branche
