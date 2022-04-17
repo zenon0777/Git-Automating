dbb52f564f7cf10ba4e58a77d8b10ddd8d1584d4
adaifi <adaifi@e2r6p13.1337.ma>
2022-04-17 22:05:52 +0000

    working on it

---
+++ automate.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+read -p {"your repo"} $repo
+read -p {"your branche"} $branche
+read -p {"your message"} $message
+
+git init
+git checkout $branche
+git add .
+git commit -m $message
+git remote add git@github.com:DAIFI-A/$repo.git
+git push -u $repo $branche
