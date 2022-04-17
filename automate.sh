2466379c298ba002faf73bd95b9b27a62a1b8279
DAIFI-A <abderrahmane.daifi-etu@etu.univh2c.ma>
2022-04-18 07:00:55 +0800

    "hello"

--- automate.sh
+++ automate.sh
@@ -5,7 +5,7 @@ read -p {"your branche"} branche
 read -p {"your message"} message
 
 git init
-git checkout $branche
+git checkout -b $branche
 git add .
 git commit -m $message
 git remote add $repo git@github.com:DAIFI-A/$repo.git
