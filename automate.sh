0525e603a5f295a6bfec64a3779ec9e16210262b
adaifi <adaifi@e2r2p7.1337.ma>
2022-04-18 19:59:25 +0000

    improve the work of the script

--- automate.sh
+++ automate.sh
@@ -3,10 +3,11 @@
 read -p {"your repo"} repo
 read -p {"your branche"} branche
 read -p {"your message"} message
+read -p "your files "  files
 
 git init
 git checkout $branche
-git add .
-git commit -m $message
+git add $files
+git commit -m "$message"
 git remote add $repo git@github.com:DAIFI-A/$repo.git
 git push -u $repo $branche
